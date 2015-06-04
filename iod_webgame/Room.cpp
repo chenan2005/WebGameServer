dcb9bd0d44d4f8e469822bd4383ab27ba90f9a64
chenan <chenan2005@gmail.com>
2015-06-04 16:46:06 +0800

    添加 iod_msg_handler类

--- iod_webgame/Room.cpp
+++ iod_webgame/Room.cpp
@@ -1,5 +1,8 @@
 #include "Room.h"
 
+REG_PB_MSG_HANDLE_BEGIN(Room)
+REG_PB_MSG_HANDLE_END(Room)
+
 Room::Room(void)
 {
 }
--- iod_webgame/Room.h
+++ iod_webgame/Room.h
@@ -1,7 +1,11 @@
 #pragma once
 
-class Room
+#include "iod_pb_msg_handler.h"
+
+class Room : public iod_pb_msg_handler
 {
+	DEC_PB_MSG_HANDLE(Room);
+
 public:
 	Room(void);
 	virtual ~Room(void);
